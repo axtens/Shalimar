3e224c82138aceacb5f2b8d93a0a9264f6ed7f67
Bruce M. Axtens <bugmagnet@outlook.com>
2019-12-09 14:49:30 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArduinoDriver;
+using ArduinoUploader;
+using Microsoft.ClearScript.JavaScript;
+
+namespace Shalimar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
